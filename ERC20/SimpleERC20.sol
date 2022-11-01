9d733e92cbe3799aac94bb96afa1e3b0454cd536
Suho Kwon <34533493+knix008@users.noreply.github.com>
2022-11-01 16:30:06 +0900

    Update SimpleERC20.sol

--- ERC20/SimpleERC20.sol
+++ ERC20/SimpleERC20.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MEGA", "MEG") {
-        initialSupply = 1000000;
-        _mint(msg.sender, initialSupply);
+contract SimpleERC20Token is ERC20 {
+    constructor() ERC20("MEGA", "MEG") {
+        _mint(msg.sender, 1000000000);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
     }
 }
