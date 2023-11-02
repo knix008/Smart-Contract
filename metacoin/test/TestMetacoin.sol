d1dd002f66511300891510f241b8b0d45c78e7eb
knix008 <suho.kwon@gmail.com>
2023-11-02 13:15:24 +0900

    Change...

--- metacoin/test/TestMetacoin.sol
+++ metacoin/test/TestMetacoin.sol
@@ -1,20 +1,19 @@
-pragma solidity >=0.4.21 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+// These files are dynamically created at test time
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/MetaCoin.sol";
 
-contract TestMetacoin {
+contract TestMetaCoin {
+
     function testInitialBalanceUsingDeployedContract() public {
         MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
         uint expected = 10000;
 
-        Assert.equal(
-            meta.getBalance(msg.sender),
-            expected,
-            "Owner should have 10000 MetaCoin initially"
-        );
+        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
     }
 
     function testInitialBalanceWithNewMetaCoin() public {
@@ -22,10 +21,7 @@ contract TestMetacoin {
 
         uint expected = 10000;
 
-        Assert.equal(
-            meta.getBalance(address(this)),
-            expected,
-            "Owner should have 10000 MetaCoin initially"
-        );
+        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
     }
+
 }
