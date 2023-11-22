203d0be8568e2947e5bf23d9135cd2b59173593c
Suho Kwon <34533493+knix008@users.noreply.github.com>
2023-11-22 20:30:56 +0900

    Add files via upload

---
+++ ERC20/ERC20Example.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: No License
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 1 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
+    }
+}
