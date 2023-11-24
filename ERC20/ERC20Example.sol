4a5b2f85c7f88ee03153abf2780367ef12dc7fff
Suho Kwon <34533493+knix008@users.noreply.github.com>
2023-11-24 20:24:06 +0900

    Update ERC20Example.sol

--- ERC20/ERC20Example.sol
+++ ERC20/ERC20Example.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: No License
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract MyToken is ERC20, Ownable {
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
@@ -13,7 +13,7 @@ contract MyToken is ERC20, ERC20Burnable, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    
+
     function decimals() public view virtual override returns (uint8) {
         return 18;
     }
