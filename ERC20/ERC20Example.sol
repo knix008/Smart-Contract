f289c35f2ba2773a70a1007f50e0db9ddb3eae88
knix008 <suho.kwon@gmail.com>
2024-09-24 16:22:28 +0900

    Add changes.

--- ERC20/ERC20Example.sol
+++ ERC20/ERC20Example.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts@4.9.3/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@4.9.3/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@4.9.3/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {
+contract MyERC20Token is ERC20, Ownable {
+    constructor() ERC20("MyERC20Token", "MTK") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
