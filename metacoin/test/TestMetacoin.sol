96707e97486cab4bb2f9c6bd164bf051b62c72fe
Shkwon <suho.kwon@gmail.com>
2025-07-12 15:49:45 +0900

    Add changes.

--- metacoin/test/TestMetacoin.sol
+++ metacoin/test/TestMetacoin.sol
@@ -8,20 +8,20 @@ import "../contracts/MetaCoin.sol";
 
 contract TestMetaCoin {
 
-    function testInitialBalanceUsingDeployedContract() public {
-        MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+  function testInitialBalanceUsingDeployedContract() public {
+    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
-        uint expected = 10000;
+    uint expected = 10000;
 
-        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-    }
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+  }
 
-    function testInitialBalanceWithNewMetaCoin() public {
-        MetaCoin meta = new MetaCoin();
+  function testInitialBalanceWithNewMetaCoin() public {
+    MetaCoin meta = new MetaCoin();
 
-        uint expected = 10000;
+    uint expected = 10000;
 
-        Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
-    }
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+  }
 
 }
