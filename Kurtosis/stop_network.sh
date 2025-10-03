6a89c6cfebcca70f3bf361b548b3e5445af0aff1
Suho Kwon <suho.kwon@gmail.com>
2025-10-03 18:41:34 +0900

    Add changes>

--- Kurtosis/stop_network.sh
+++ Kurtosis/stop_network.sh
@@ -9,12 +9,16 @@ echo "======================================"
 ENCLAVE_NAME="ethereum-net"
 
 # Stop the enclave
-echo -e "\n[1/2] Stopping enclave: $ENCLAVE_NAME..."
+echo -e "\n[1/3] Stopping enclave: $ENCLAVE_NAME..."
 kurtosis enclave stop $ENCLAVE_NAME 2>/dev/null || echo "Enclave not running"
 
 # Remove the enclave
-echo -e "\n[2/2] Removing enclave: $ENCLAVE_NAME..."
+echo -e "\n[2/3] Removing enclave: $ENCLAVE_NAME..."
 kurtosis enclave rm -f $ENCLAVE_NAME 2>/dev/null || echo "Enclave already removed"
+
+# Remove all containers
+echo -e "\n[3/3] Removing all containers..."
+docker container prune -f || echo "No containers to remove"
 
 echo -e "\n======================================"
 echo "Network stopped and cleaned up!"
